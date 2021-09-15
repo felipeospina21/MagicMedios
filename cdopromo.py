e70ad91d1d5122b72a6688622b78fab6c3f1bb0b
Felipe Ospina <felipeospina21@hotmail.com>
2021-09-15 12:26:38 -0500

    change regex function for references

---
+++ cdopromo.py
@@ -0,0 +1,5 @@
+
+from get_data import Get_Data
+
+def get_cdo_promo_data(suppliers_dict, prs, references):
+    pass
--- utils.py
+++ utils.py
@@ -1,15 +1,33 @@
 import re
-from pptx import Presentation
+# from pptx import Presentation
 from pptx.util import Pt, Cm
 from pptx.enum.text import PP_ALIGN
 
+# def create_supplier_ref_list(ref,suppliers_dict):
+#     if re.search('-', ref):
+#         suppliers_dict['cat_promo'].append(ref)
+#     elif re.search('^[^NWnw]', ref) and re.search('^[a-zA-Z]{2}[0-9]{4}$|^[a-zA-Z]{3}[0-9]{3}$', ref):
+#         suppliers_dict['mp_promo'].append(ref)
+#     elif re.search(' +', ref):
+#         suppliers_dict['promo_op'].append(ref)
+#     elif re.search('^NW|^nw', ref):
+#         suppliers_dict['nw_promo'].append(ref)
+    
+#     return suppliers_dict
+
 def create_supplier_ref_list(ref,suppliers_dict):
-    if re.search('-', ref):
-        suppliers_dict['cat_promo'].append(ref)
-    elif re.search('^[^NWnw]', ref) and re.search('^[a-zA-Z]{2}[0-9]{4}$|^[a-zA-Z]{3}[0-9]{3}$', ref):
-        suppliers_dict['mp_promo'].append(ref)
-    elif re.search(' +', ref):
-        suppliers_dict['promo_op'].append(ref)
+    if re.search('^CP|^cp]', ref):
+        split_ref = ref.split("CP")
+        suppliers_dict['cat_promo'].append(split_ref[1])
+    elif re.search('^MP|^mp]', ref):
+        split_ref = ref.split("MP")
+        suppliers_dict['mp_promo'].append(split_ref[1])
+    elif re.search('^PO|^po]', ref):
+        split_ref = ref.split("PO")
+        suppliers_dict['promo_op'].append(split_ref[1])
+    elif re.search('^CD|^cd', ref):
+        split_ref = ref.split("CD")
+        suppliers_dict['cdo_promo'].append(split_ref[1])
     elif re.search('^NW|^nw', ref):
         suppliers_dict['nw_promo'].append(ref)
     
