234736438d82706b69d2259220a30e762df585b6
Felipe Ospina <felipeospina21@hotmail.com>
2021-09-16 16:45:54 -0500

    add production api for cdo

--- cdopromo.py
+++ cdopromo.py
@@ -11,12 +11,12 @@ def get_cdo_promo_data(suppliers_dict, prs, references):
     for ref in suppliers_dict['cdo_promo']:
         idx = data.get_original_ref_list_idx(ref)
         count = idx + 1
-        url = f"http://api.argentina.cdo.dev.yellowspot.com.ar/v1/products/{ref}?auth_token={auth_token}"
+        url = f"http://api.colombia.cdopromocionales.com/v1/products/{ref}?auth_token={auth_token}"
         result = get_api_data(url)
         title = result["name"]
         desc = result["description"]
         colors = result["variants"]
-        img_src = colors[0]["picture"]["medium"]
+        img_src = colors[0]["detail_picture"]["medium"]
         q_colores = len(colors)
         data.create_title(title, idx, count, ref)
         data.create_quantity_table(ref, idx)
