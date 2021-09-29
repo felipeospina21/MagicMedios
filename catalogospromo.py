28e3b18446bec295e1a3386e6d59077d7043ce88
Felipe Ospina <felipeospina21@hotmail.com>
2021-09-29 14:29:19 -0500

    add ref validation before click in first result

--- catalogospromo.py
+++ catalogospromo.py
@@ -13,25 +13,30 @@ def get_cat_promo_data(suppliers_dict, prs, references):
             count = idx + 1
             # data.search_ref(ref,'productos')
             search_input = data.get_element_with_xpath("//input[@id='productos']")
-            
             data.stop_loading()
             data.send_keys(search_input, ref)
-            data.click_first_result("//div[@id='backTable']/div[1]/div[1]/a[1]", ref)
-            time.sleep(1)
-            data.create_quantity_table(ref, idx)
-            
-            header_text = data.get_title_and_subtitle(header_xpath, 0, 0, ref)
-            data.create_title(header_text[0], idx, count, ref)
-            desc_list = data.get_description(f"{header_xpath}/p[2]", ref)
-            data.create_description(desc_list, idx, ref)
-            pack_info_list = data.get_package_info(ref)
-            data.create_package_info(pack_info_list[0], pack_info_list[1], pack_info_list[2], pack_info_list[3], idx, ref)
-            
-            colors_len = data.get_elements_len_with_xpath("//tr[@class='titlesRow']/following-sibling::tr")
-            data.create_inventory_table(colors_len, "//table[@class='tableInfoProd']", idx, ref)
-            
-            img_src = data.get_img("//img[@id='img_01']", ref)
-            data.create_img(img_src, idx, 8, 8, ref)
+            search_results_xpath = "//div[@id='backTable']/div[1]"
+            colors_q = data.get_elements_len_with_xpath(f"{search_results_xpath}/child::div")
+            for i in range(1, colors_q + 1):
+                result_ref = data.get_element_with_xpath(f"{search_results_xpath}/div[{i}]/div[2]/p[1]").text 
+                if ref == result_ref:
+                    data.click_first_result(f"{search_results_xpath}/div[{i}]/a[1]", ref)
+                    time.sleep(1)
+                    data.create_quantity_table(ref, idx)
+                    
+                    header_text = data.get_title_and_subtitle(header_xpath, 0, 0, ref)
+                    data.create_title(header_text[0], idx, count, ref)
+                    desc_list = data.get_description(f"{header_xpath}/p[2]", ref)
+                    data.create_description(desc_list, idx, ref)
+                    pack_info_list = data.get_package_info(ref)
+                    data.create_package_info(pack_info_list[0], pack_info_list[1], pack_info_list[2], pack_info_list[3], idx, ref)
+                    
+                    colors_len = data.get_elements_len_with_xpath("//tr[@class='titlesRow']/following-sibling::tr")
+                    data.create_inventory_table(colors_len, "//table[@class='tableInfoProd']", idx, ref)
+                    
+                    img_src = data.get_img("//img[@id='img_01']", ref)
+                    data.create_img(img_src, idx, 8, 8, ref)
+                    break
         except:
             print(f"\nNo se pudo obtener la información del a ref {ref}")
 
