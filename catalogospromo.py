d1dc80e71123402c35f354c38db1371cca60ac47
Felipe Ospina <felipeospina21@hotmail.com>
2022-02-10 17:45:08 -0500

    fix bug from last update

--- catalogospromo.py
+++ catalogospromo.py
@@ -1,13 +1,13 @@
-
 from get_data import Get_Data
 from utils import measures
 import time
 
+
 def get_cat_promo_data(suppliers_dict, prs, references):
-    data = Get_Data('cat_promo', prs, references, measures)
-    data.execute_driver('https://www.catalogospromocionales.com/')
+    data = Get_Data("cat_promo", prs, references, measures)
+    data.execute_driver("https://www.catalogospromocionales.com/")
     header_xpath = "//div[@class='hola']"
-    for ref in suppliers_dict['cat_promo']:
+    for ref in suppliers_dict["cat_promo"]:
         try:
             idx = data.get_original_ref_list_idx(ref)
             count = idx + 1
@@ -16,24 +16,39 @@ def get_cat_promo_data(suppliers_dict, prs, references):
             data.stop_loading()
             data.send_keys(search_input, ref)
             search_results_xpath = "//div[@id='backTable']/div[1]"
-            colors_q = data.get_elements_len_with_xpath(f"{search_results_xpath}/child::div")
+            colors_q = data.get_elements_len_with_xpath(
+                f"{search_results_xpath}/child::div"
+            )
             for i in range(1, colors_q + 1):
-                result_ref = data.get_element_with_xpath(f"{search_results_xpath}/div[{i}]/div[2]/p[1]").text.upper()
+                result_ref = data.get_element_with_xpath(
+                    f"{search_results_xpath}/div[{i}]/div[2]/p[1]"
+                ).text.upper()
                 if ref == result_ref:
-                    data.click_first_result(f"{search_results_xpath}/div[{i}]/a[1]", ref)
-                    time.sleep(1)
+                    data.click_first_result(f"{search_results_xpath}/div[{i}]/a[1]")
+                    time.sleep(2)
                     data.create_quantity_table(ref, idx)
-                    
+
                     header_text = data.get_title_and_subtitle(header_xpath, 0, 0, ref)
                     data.create_title(header_text[0], idx, count, ref)
                     desc_list = data.get_description(f"{header_xpath}/p[2]", ref)
                     data.create_description(desc_list, idx, ref)
                     pack_info_list = data.get_package_info(ref)
-                    data.create_package_info(pack_info_list[0], pack_info_list[1], pack_info_list[2], pack_info_list[3], idx, ref)
-                    
-                    colors_len = data.get_elements_len_with_xpath("//tr[@class='titlesRow']/following-sibling::tr")
-                    data.create_inventory_table(colors_len, "//table[@class='tableInfoProd']", idx, ref)
-                    
+                    data.create_package_info(
+                        pack_info_list[0],
+                        pack_info_list[1],
+                        pack_info_list[2],
+                        pack_info_list[3],
+                        idx,
+                        ref,
+                    )
+
+                    colors_len = data.get_elements_len_with_xpath(
+                        "//tr[@class='titlesRow']/following-sibling::tr"
+                    )
+                    data.create_inventory_table(
+                        colors_len, "//table[@class='tableInfoProd']", idx, ref
+                    )
+
                     img_src = data.get_img("//img[@id='img_01']", ref)
                     data.create_img(img_src, idx, 8, 8, ref)
                     break
--- mppromos.py
+++ mppromos.py
@@ -2,27 +2,42 @@ from get_data import Get_Data
 from utils import measures
 import time
 
+
 def get_mp_promo_data(suppliers_dict, prs, references):
-    data = Get_Data('mp_promo', prs, references, measures)
-    data.execute_driver('https://www.mppromocionales.com/')
-    for ref in suppliers_dict['mp_promo']:
+    data = Get_Data("mp_promo", prs, references, measures)
+    data.execute_driver("https://www.mppromocionales.com/")
+    for ref in suppliers_dict["mp_promo"]:
         try:
             idx = data.get_original_ref_list_idx(ref)
+            print(idx)
             count = idx + 1
             # data.search_ref(ref,'mat-input-0')
             search_input = data.get_element_with_xpath("//input[@id='mat-input-0']")
             data.send_keys(search_input, ref)
             time.sleep(3)
             data.create_quantity_table(ref, idx)
-            title_text = data.get_title_with_xpath("//h1[@class='g-font-size-20 g-font-weight-600']", ref)
-            subtitle_text = data.get_subtitle_with_xpath("//div[@class='g-font-size-18 g-mb-15']", ref)
+            title_text = data.get_title_with_xpath(
+                "//h1[@class='g-font-size-20 g-font-weight-600']", ref
+            )
+            subtitle_text = data.get_subtitle_with_xpath(
+                "//div[@class='g-font-size-18 g-mb-15']", ref
+            )
             data.create_title(title_text, idx, count, ref)
-            data.create_subtitle(subtitle_text, idx, ref)
-            
-            desc_list = data.get_description("//ul[@class='g-mb-16 g-ml-20 g-pl-0 g-font-size-14']/child::li", ref)
+            data.create_subtitle(subtitle_text, idx)
+
+            desc_list = data.get_description(
+                "//ul[@class='g-mb-16 g-ml-20 g-pl-0 g-font-size-14']/child::li", ref
+            )
             data.create_description(desc_list, idx, ref)
-            colors_len = data.get_elements_len_with_xpath("//mat-table[@class='w-100 inventory-tabla mat-table']/child::mat-row")
-            data.create_inventory_table(colors_len, "//mat-table[@class='w-100 inventory-tabla mat-table']", idx, ref)
+            colors_len = data.get_elements_len_with_xpath(
+                "//mat-table[@class='w-100 inventory-tabla mat-table']/child::mat-row"
+            )
+            data.create_inventory_table(
+                colors_len,
+                "//mat-table[@class='w-100 inventory-tabla mat-table']",
+                idx,
+                ref,
+            )
 
             img_src = data.get_img("//img[@class='ng-star-inserted']", ref)
             data.create_img(img_src, idx, 10.39, 7.4, ref)
@@ -30,4 +45,3 @@ def get_mp_promo_data(suppliers_dict, prs, references):
             print(f"\nNo se pudo obtener la información del a ref {ref}")
 
     data.close_driver()
-
