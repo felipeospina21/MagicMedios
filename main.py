daeb4a71af6a67c6832bfc016671da330f8f7e6d
Felipe Ospina <51976045+felipeospina21@users.noreply.github.com>
2022-08-01 19:57:37 -0500

    add User class

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import time
 import os
 import getpass
 
+from new_quotation import New_Quotation
+
 # Variables
 start_time = time.time()
 username = getpass.getuser()
@@ -26,148 +28,172 @@ title_slide_layout = prs.slide_layouts[6]
 locale.setlocale(locale.LC_TIME, "")
 
 print("-------------****-------------- ")
-client = input("Ingrese nombre cliente: ").title()
-company = input("Ingrese nombre empresa: ").upper()
-rep_name = input(
-    "Ingrese nombre asesor comercial (sergio, carlos, ale, juli, com): "
-).lower()
-reference = (
-    input("Ingrese referencias a consultar (separadas por coma): ").upper().split(",")
-)
-strip_reference = [ref.strip() for ref in reference]
-ref_q = len(reference)
+program_flow = input("Digite el número de la acción que desea realizar: \n[1] Crear nueva cotizacion \n[2] Crear nuevo usuario\n> ")
 
-# Load header info
-if username == "felipe" or username == "felip":
-    consecutivo_path = f"{file_path}/data/consecutivo.txt"
+if program_flow == '1':
+  client = input("Ingrese nombre cliente: ").title()
+  company = input("Ingrese nombre empresa: ").upper()
+  rep_name = input(
+      "Ingrese nombre asesor comercial (sergio, carlos, ale, juli, com): "
+  ).lower()
+  reference = (
+      input("Ingrese referencias a consultar (separadas por coma): ").upper().split(",")
+  )
+  strip_reference = [ref.strip() for ref in reference]
+  ref_q = len(reference)
+
+  # Load header info
+  if username == "felipe" or username == "felip":
+      consecutivo_path = f"{file_path}/data/consecutivo.txt"
+  else:
+      consecutivo_path = f"{cotizaciones_path}/Z consecutivo.txt"
+
+
+  file = open(consecutivo_path, "r")
+  consecutivo = file.readline().strip()
+  file.close()
+
+  # Routes
+  if username == "felipe" or username == "felip":
+      comercial_path = f"{file_path}/data/comercial.txt"
+      carlos_path = f"{file_path}/data/carlos.txt"
+      sergio_path = f"{file_path}/data/sergio.txt"
+      juliana_path = f"{file_path}/data/juliana.txt"
+      alejandra_path = f"{file_path}/data/sergio.txt"
+      save_path = f"./cotizaciones/cotización_{company}.pptm"
+      footer_path = f"{file_path}/data/data.txt"
+  else:
+      comercial_path = f"{cotizaciones_path}/data/comercial.txt"
+      carlos_path = f"{cotizaciones_path}/data/carlos.txt"
+      sergio_path = f"{cotizaciones_path}/data/sergio.txt"
+      juliana_path = f"{cotizaciones_path}/data/juliana.txt"
+      alejandra_path = f"{cotizaciones_path}/data/alejandra.txt"
+      save_path = f"{cotizaciones_path}/Cotización N°{consecutivo} - {company} - Magic Medios SAS.pptm"
+      footer_path = f"{cotizaciones_path}/data/data.txt"
+
+  # Get info asesor comercial
+  if rep_name == "sergio":
+      file = open(sergio_path, "r")
+  elif rep_name == "carlos":
+      file = open(carlos_path, "r")
+  elif rep_name == "juli":
+      file = open(juliana_path, "r")
+  elif rep_name == "ale":
+      file = open(alejandra_path, "r")
+  else:
+      file = open(comercial_path, "r")
+      # file = open(comercial_path, "r")
+
+  representative = file.readline()
+  contact = file.readline()
+  email = file.readline()
+  file.close()
+
+  # Get footer info
+  file = open(footer_path, "r")
+  address = file.readline()
+  web = file.readline()
+  file.close()
+
+  suppliers = {
+      "cat_promo": [],
+      "mp_promo": [],
+      "promo_op": [],
+      "nw_promo": [],
+      "cdo_promo": [],
+      "best_stock": [],
+  }
+
+  # Add slides with logo and footer
+  num_of_slides = len(reference)
+  for i in range(0, num_of_slides + 1):
+      prs.slides.add_slide(title_slide_layout)
+      pic = prs.slides[i].shapes.add_picture(
+          "./images/logo.jpg", left=Cm(1), top=Cm(0.5), width=Cm(8.9), height=Cm(1.7)
+      )
+      # footer = prs.slides[idx].shapes.add_textbox(left=Cm(7.5), top=Cm(22.3), width=Cm(14),height=Cm(3))
+      footer = prs.slides[i].shapes.add_textbox(
+          left=Cm(0.5), top=Cm(22.8), width=Cm(18), height=Cm(1)
+      )
+      tf_footer = footer.text_frame
+      text_frame_paragraph(
+          tf_footer, f"{address} {contact} {email} {web}", 7, False, True
+      )
+
+  ## Firste Slide
+  # Add Header
+  txBox = prs.slides[0].shapes.add_textbox(
+      left=Cm(12), top=Cm(-0.5), width=Cm(6.6), height=Cm(6)
+  )
+  tf = txBox.text_frame
+  text_frame_paragraph(tf, f"{hoy.day} {hoy.strftime('%B')} de {hoy.year}", 14)
+  text_frame_paragraph(tf, f"Cot N°{consecutivo}", 14)
+  text_frame_paragraph(tf, "", 11)
+  text_frame_paragraph(tf, "Asesor Comercial", 11)
+  text_frame_paragraph(tf, f"{representative} {contact} {email}", 11)
+
+  # Add Client name
+  header = prs.slides[0].shapes.add_textbox(
+      left=Cm(1), top=Cm(1.8), width=Cm(6.4), height=Cm(2)
+  )
+  tf_header = header.text_frame
+  text_frame_paragraph(tf_header, f"Señor(a) {client}.", 14, True)
+  text_frame_paragraph(tf_header, company, 14, True)
+
+  # Create each suppliers ref list
+  for ref in strip_reference:
+      suppliers = create_supplier_ref_list(ref, suppliers)
+
+  # Scrapp Data
+  if len(suppliers["cat_promo"]) != 0:
+      get_cat_promo_data(suppliers, prs, strip_reference)
+  if len(suppliers["mp_promo"]) != 0:
+      get_mp_promo_data(suppliers, prs, strip_reference)
+  if len(suppliers["promo_op"]) != 0:
+      get_promo_op__data(suppliers, prs, strip_reference)
+  if len(suppliers["nw_promo"]) != 0:
+      get_nw_promo_data(suppliers, prs, strip_reference)
+  if len(suppliers["cdo_promo"]) != 0:
+      get_cdo_promo_data(suppliers, prs, strip_reference)
+
+  # Condiciones comerciales
+  pic = prs.slides[num_of_slides].shapes.add_picture(
+      "./images/condiciones.jpg",
+      left=Cm(1),
+      top=Cm(4.5),
+      width=Cm(17.27),
+      height=Cm(16.66),
+  )
+
+
+  prs.save(save_path)
+
+  # Guarda consecutivo
+  nuevo_consecutivo = int(consecutivo) + 1
+  file = open(consecutivo_path, "w")
+  file.write(f"{nuevo_consecutivo}\n")
+  file.close()
+
+  total_time = "{:.2f}".format((time.time() - start_time) / 60)
+  print(f"-------- Proceso Finalizado en {total_time} minutos --------")
+
 else:
-    consecutivo_path = f"{cotizaciones_path}/Z consecutivo.txt"
 
+  suppliers = {
+      "cat_promo": [],
+      "mp_promo": [],
+      "promo_op": [],
+      "nw_promo": [],
+      "cdo_promo": [],
+      "best_stock": [],
+  }
 
-file = open(consecutivo_path, "r")
-consecutivo = file.readline().strip()
-file.close()
+  quotation = New_Quotation( file_path, prs, title_slide_layout)
+  quotation.set_slides()
+  quotation.add_header(hoy)
+  quotation.add_client_name()
+  suppliers_list = quotation.create_suppliers_ref_list(suppliers)
+  quotation.scrapp_data(suppliers_list, suppliers)
+  quotation.create_commercial_policy()
+  quotation.save()
 
-# Routes
-if username == "felipe" or username == "felip":
-    comercial_path = f"{file_path}/data/comercial.txt"
-    carlos_path = f"{file_path}/data/carlos.txt"
-    sergio_path = f"{file_path}/data/sergio.txt"
-    juliana_path = f"{file_path}/data/juliana.txt"
-    alejandra_path = f"{file_path}/data/sergio.txt"
-    save_path = f"./cotizaciones/cotización_{company}.pptm"
-    footer_path = f"{file_path}/data/data.txt"
-else:
-    comercial_path = f"{cotizaciones_path}/data/comercial.txt"
-    carlos_path = f"{cotizaciones_path}/data/carlos.txt"
-    sergio_path = f"{cotizaciones_path}/data/sergio.txt"
-    juliana_path = f"{cotizaciones_path}/data/juliana.txt"
-    alejandra_path = f"{cotizaciones_path}/data/alejandra.txt"
-    save_path = f"{cotizaciones_path}/Cotización N°{consecutivo} - {company} - Magic Medios SAS.pptm"
-    footer_path = f"{cotizaciones_path}/data/data.txt"
-
-# Get info asesor comercial
-if rep_name == "sergio":
-    file = open(sergio_path, "r")
-elif rep_name == "carlos":
-    file = open(carlos_path, "r")
-elif rep_name == "juli":
-    file = open(juliana_path, "r")
-elif rep_name == "ale":
-    file = open(alejandra_path, "r")
-else:
-    file = open(comercial_path, "r")
-    # file = open(comercial_path, "r")
-
-representative = file.readline()
-contact = file.readline()
-email = file.readline()
-file.close()
-
-# Get footer info
-file = open(footer_path, "r")
-address = file.readline()
-web = file.readline()
-file.close()
-
-suppliers = {
-    "cat_promo": [],
-    "mp_promo": [],
-    "promo_op": [],
-    "nw_promo": [],
-    "cdo_promo": [],
-    "best_stock": [],
-}
-
-# Add slides with logo and footer
-num_of_slides = len(reference)
-for i in range(0, num_of_slides + 1):
-    prs.slides.add_slide(title_slide_layout)
-    pic = prs.slides[i].shapes.add_picture(
-        "./images/logo.jpg", left=Cm(1), top=Cm(0.5), width=Cm(8.9), height=Cm(1.7)
-    )
-    # footer = prs.slides[idx].shapes.add_textbox(left=Cm(7.5), top=Cm(22.3), width=Cm(14),height=Cm(3))
-    footer = prs.slides[i].shapes.add_textbox(
-        left=Cm(0.5), top=Cm(22.8), width=Cm(18), height=Cm(1)
-    )
-    tf_footer = footer.text_frame
-    text_frame_paragraph(
-        tf_footer, f"{address} {contact} {email} {web}", 7, False, True
-    )
-
-## Firste Slide
-# Add Header
-txBox = prs.slides[0].shapes.add_textbox(
-    left=Cm(12), top=Cm(-0.5), width=Cm(6.6), height=Cm(6)
-)
-tf = txBox.text_frame
-text_frame_paragraph(tf, f"{hoy.day} {hoy.strftime('%B')} de {hoy.year}", 14)
-text_frame_paragraph(tf, f"Cot N°{consecutivo}", 14)
-text_frame_paragraph(tf, "", 11)
-text_frame_paragraph(tf, "Asesor Comercial", 11)
-text_frame_paragraph(tf, f"{representative} {contact} {email}", 11)
-
-# Add Client name
-header = prs.slides[0].shapes.add_textbox(
-    left=Cm(1), top=Cm(1.8), width=Cm(6.4), height=Cm(2)
-)
-tf_header = header.text_frame
-text_frame_paragraph(tf_header, f"Señor(a) {client}.", 14, True)
-text_frame_paragraph(tf_header, company, 14, True)
-
-# Create each suppliers ref list
-for ref in strip_reference:
-    suppliers = create_supplier_ref_list(ref, suppliers)
-
-# Scrapp Data
-if len(suppliers["cat_promo"]) != 0:
-    get_cat_promo_data(suppliers, prs, strip_reference)
-if len(suppliers["mp_promo"]) != 0:
-    get_mp_promo_data(suppliers, prs, strip_reference)
-if len(suppliers["promo_op"]) != 0:
-    get_promo_op__data(suppliers, prs, strip_reference)
-if len(suppliers["nw_promo"]) != 0:
-    get_nw_promo_data(suppliers, prs, strip_reference)
-if len(suppliers["cdo_promo"]) != 0:
-    get_cdo_promo_data(suppliers, prs, strip_reference)
-
-# Condiciones comerciales
-pic = prs.slides[num_of_slides].shapes.add_picture(
-    "./images/condiciones.jpg",
-    left=Cm(1),
-    top=Cm(4.5),
-    width=Cm(17.27),
-    height=Cm(16.66),
-)
-
-
-prs.save(save_path)
-
-# Guarda consecutivo
-nuevo_consecutivo = int(consecutivo) + 1
-file = open(consecutivo_path, "w")
-file.write(f"{nuevo_consecutivo}\n")
-file.close()
-
-total_time = "{:.2f}".format((time.time() - start_time) / 60)
-print(f"-------- Proceso Finalizado en {total_time} minutos --------")
---
+++ new_quotation.py
@@ -0,0 +1,133 @@
+from utils import text_frame_paragraph, create_supplier_ref_list
+from catalogospromo import get_cat_promo_data
+from mppromos import get_mp_promo_data
+from nwpromo import get_nw_promo_data
+from promoop import get_promo_op__data
+from cdopromo import get_cdo_promo_data
+from pptx.util import Cm
+import os
+
+class New_Quotation:
+
+  def __init__(self, path, prs, title_slide_layout) -> None:
+    self.user_path = f"{path}/data"
+    self.users = self.get_users()
+    self.client = input("Ingrese nombre cliente: ").title()
+    self.company = input("Ingrese nombre empresa: ").upper()
+    self.user = input(
+        f"Ingrese nombre asesor comercial ({', '.join(self.users)}): "
+    ).lower()
+    self.reference = (
+        input("Ingrese referencias a consultar (separadas por coma): ").upper().split(",")
+    )
+
+    self.consecutive_path = f"{path}/data/consecutivo.txt"
+    self.footer_path = f"{path}/data/data.txt"
+    self.consecutive = self.get_consecutive()
+    self.rep_name, self.phone, self.email = self.get_contact_info()
+    self.address, self.web = self.get_footer()
+    self.prs = prs
+    self.title_slide_layout = title_slide_layout
+
+    self.strip_reference = [ref.strip() for ref in self.reference]
+    self.ref_q = len(self.reference)
+    self.save_path = f"./cotizaciones/cotización_{self.company}.pptm"
+
+
+  def get_users(self):
+    users = []
+    exclude = ['consecutivo', 'data']
+    for x in os.listdir(self.user_path):
+      if x.endswith('.txt'):
+        user = x.split('.txt')[0]
+        if user not in exclude:
+          users.append(user)
+
+    return users
+
+  def get_consecutive(self):
+    file = open(self.consecutive_path, "r")
+    consecutive = file.readline().strip()
+    file.close()
+    return consecutive
+
+  def get_contact_info(self):
+    file = open(f'{self.user_path}/{self.user}.txt', 'r')
+    rep_name = file.readline()
+    phone = file.readline()
+    email = file.readline()
+    file.close()
+
+    return rep_name, phone, email
+
+  def get_footer(self):
+    file = open(self.footer_path, "r")
+    address = file.readline()
+    web = file.readline()
+    file.close()
+    return address, web
+
+  def set_slides(self):
+    
+    for i in range(0, self.ref_q + 1):
+      self.prs.slides.add_slide(self.title_slide_layout)
+      self.prs.slides[i].shapes.add_picture(
+          "./images/logo.jpg", left=Cm(1), top=Cm(0.5), width=Cm(8.9), height=Cm(1.7)
+      )
+      # footer = prs.slides[idx].shapes.add_textbox(left=Cm(7.5), top=Cm(22.3), width=Cm(14),height=Cm(3))
+      footer = self.prs.slides[i].shapes.add_textbox(
+          left=Cm(0.5), top=Cm(22.8), width=Cm(18), height=Cm(1)
+      )
+      tf_footer = footer.text_frame
+      text_frame_paragraph(
+          tf_footer, f"{self.address} {self.phone} {self.email} {self.web}", 7, False, True
+      )
+
+  def add_header(self, today):
+    txBox = self.prs.slides[0].shapes.add_textbox(
+      left=Cm(12), top=Cm(-0.5), width=Cm(6.6), height=Cm(6)
+  )
+    tf = txBox.text_frame
+    text_frame_paragraph(tf, f"{today.day} {today.strftime('%B')} de {today.year}", 14)
+    text_frame_paragraph(tf, f"Cot N°{self.consecutive}", 14)
+    text_frame_paragraph(tf, "", 11)
+    text_frame_paragraph(tf, "Asesor Comercial", 11)
+    text_frame_paragraph(tf, f"{self.rep_name} {self.phone} {self.email}", 11)
+
+  def add_client_name(self):
+    header = self.prs.slides[0].shapes.add_textbox(
+      left=Cm(1), top=Cm(1.8), width=Cm(6.4), height=Cm(2)
+  )
+    tf_header = header.text_frame
+    text_frame_paragraph(tf_header, f"Señor(a) {self.client}.", 14, True)
+    text_frame_paragraph(tf_header, self.company, 14, True)
+  
+  def create_suppliers_ref_list(self, suppliers):
+    for ref in self.strip_reference:
+      suppliers_list = create_supplier_ref_list(ref, suppliers)
+    
+    return suppliers_list
+
+  def scrapp_data(self, suppliers_list, suppliers_dict):
+    if len(suppliers_list["cat_promo"]) != 0:
+      get_cat_promo_data(suppliers_dict, self.prs, self.strip_reference)
+    if len(suppliers_list["mp_promo"]) != 0:
+        get_mp_promo_data(suppliers_dict, self.prs, self.strip_reference)
+    if len(suppliers_list["promo_op"]) != 0:
+        get_promo_op__data(suppliers_dict, self.prs, self.strip_reference)
+    if len(suppliers_list["nw_promo"]) != 0:
+        get_nw_promo_data(suppliers_dict, self.prs, self.strip_reference)
+    if len(suppliers_list["cdo_promo"]) != 0:
+        get_cdo_promo_data(suppliers_dict, self.prs, self.strip_reference)
+  
+  def create_commercial_policy(self):
+    self.prs.slides[self.ref_q].shapes.add_picture(
+      "./images/condiciones.jpg",
+      left=Cm(1),
+      top=Cm(4.5),
+      width=Cm(17.27),
+      height=Cm(16.66),
+  )
+
+  def save(self):
+    self.prs.save(self.save_path)
